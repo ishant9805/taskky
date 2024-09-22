5de684519cb5bfedf10435e199e96944d81f5fa3
ishant9805 <ishant9805@gmail.com>
2024-09-22 17:26:29 +0530

    Connect db and models

---
+++ application/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+from .database import db
+from sqlalchemy.sql import func
+
+class Users(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    hash = db.Column(db.String, nullable=False)
+    tasks = db.relationship("tasks", secondary="userstask")
+
+class Tasks(db.Model):
+    __tablename__ = "tasks"
+    task_id =  db.Column(db.Integer, autoincrement=True, primary_key=True)
+    task = db.Column(db.String, nullable=False)
+    task_description = db.Column(db.String)
+    completed = db.Column(db.Boolean, default=0)
+    due_date = db.Column(db.Datetime)
+    task_created = db.Column(db.Datetime, default=func.now())
+
+class Userstask(db.Model):
+    __tablename__ = "userstask"
+    conn_id =  db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("tasks.task_id"), primary_key=True)
